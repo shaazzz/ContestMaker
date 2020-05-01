b8ebfa6786ad3756b8400550b05e3aca3fe2f828
ghmikaeel@gmail.com <ghmikaeel@gmail.com>
2020-05-01 20:21:09 +0430

    debug update problemset

--- addPrivateContest.php
+++ addPrivateContest.php
@@ -30,8 +30,8 @@ $inputs = array("username", "password", "contestAddressPrefix", "contestId", "de
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-require __DIR__ . '/Models/problemset.php';
 require __DIR__ . '/data/defines.php';
+require __DIR__ . '/Models/problemset.php';
 require __DIR__ . '/CodeforcesUserApi.php';
 
 foreach ($inputs as $input) {
--- updateProblemset.php
+++ updateProblemset.php
@@ -1,5 +1,7 @@
 <?php
-
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+require __DIR__ . '/data/defines.php';
 require __DIR__ . '/CodeforcesApi.php';
 require __DIR__ . '/Models/problemset.php';
 
@@ -11,10 +13,13 @@ $legends = json_decode(file_get_contents("data/legends.txt"), true);
 $seen = array();
 
 foreach ($legends as $person) {
-    $submitions = $cfApi->request("user.status", array("handle" => $person))['result'];
+    $submitions = json_decode($cfApi->request("user.status", array("handle" => $person))['result'], true);
+
+    echo count($submitions)."<br>";/*
     foreach ($submitions as $sub) {
         if ($sub["verdict"] == "OK") {
-            if ($seen[$sub["id"]] == null) {
+
+            /*if ($seen[$sub["id"]] == null) {
                 $seen[$sub["id"]] = true;
                 problemset::addProblem($sub["id"],
                     $sub["problem"]["contestId"] . $sub["problem"]["index"],
@@ -24,6 +29,6 @@ foreach ($legends as $person) {
             }
             problemset::addUserSolved($sub["id"], true);
         }
-    }
+    }*/
 }
 problemset::update();
