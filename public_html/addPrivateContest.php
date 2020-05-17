27f6187789a661adb3f02d06c3dd92b25edc735d
ghmikaeel@gmail.com <ghmikaeel@gmail.com>
2020-05-17 14:01:03 +0430

    add css to addprivatecontest.php

--- public_html/addPrivateContest.php
+++ public_html/addPrivateContest.php
@@ -1,101 +1,154 @@
-<?php
-$inputs = array("username", "password", "contestAddressPrefix", "contestId", "defaultDifficulty", "fromProblem", "toProblem", "prior");
-
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-
-try {
-    chdir('..');
-    require __DIR__ . '/../data/defines.php';
-    require __DIR__ . '/../Models/problemset.php';
-    require __DIR__ . '/../Models/CodeforcesUserApi.php';
-
-    foreach ($inputs as $input) {
-        if (!isset($_POST[$input])) {
-            throw new Exception("$input وارد نشده است ");
-        }
-    }
-    if (!is_numeric($_POST["prior"]) || (float)$_POST["prior"] < 0 || (float)$_POST["prior"] > 0.5) {
-        throw new Exception("prior باید بین ۰ و ۰.۵ باشد!");
-    }
-    $api = new CodeforcesUserApi();
-    if (strlen($_POST["username"]) > 0) {
-        $api->login($_POST["username"], $_POST["password"]);
-    } else {
-        $api->login(CODEFORCES_USERNAME, CODEFORCES_PASSWORD);
-    }
-    $L = ord($_POST["fromProblem"]) - ord('A');
-    $R = ord($_POST["toProblem"]) - ord('A');
-    if ($L < 0 || $L >= 26) {
-        throw new Exception("(from problem index) به درستی وارد نشده است!");
-    }
-    if ($R < $L || $R >= 26) {
-        throw new Exception("(to problem index) به درستی وارد نشده است!");
-    }
-    $problemQueries = $api->getContestProblemQueries($_POST["contestId"], $_POST["contestAddressPrefix"]);
-    problemset::readFromFile();
-    $allTags = json_decode(file_get_contents("data/allTags.txt"), true);
-    $additionalTags = array();
-    if (strlen($_POST['additionalTags']) > 0) {
-        $additionalTags = explode(',', strtolower($_POST['additionalTags']));
-        foreach ($additionalTags as $tag) {
-            if (!in_array($tag, $allTags)) {
-                throw new Exception("تگ $tag  به درستی وارد نشده است!");
-            }
-        }
-    }
-    for ($i = $L; $i <= $R; $i++) {
-        if (!isset($problemQueries[$i])) {
-            throw new Exception("خطا در دریافت اطلاعات سوال " . chr($i + ord('A')));
-        }
-    }
-
-    for ($i = $L; $i <= $R; $i++) {
-        $data = $api->getProblemArrayData($problemQueries[$i]);
-        if (!isset($data['rating'])) {
-            $data['rating'] = $_POST['defaultDifficulty'];
-        }
-        $problemId = problemset::addProblem($problemQueries[$i],
-            array_values(array_unique(array_merge(json_decode($data['tags'], true), $additionalTags)))
-            , (int)$data['rating'], (float)$_POST["prior"], false);
-        if(strlen($_POST["username"])>0) {
-            problemset::addUserLiked(strtolower($_POST["username"]), $problemId);
-        }
-    }
-} catch (Exception $e) {
-    echo "<h3 dir=\"rtl\"> خطا: " . $e->getMessage();
-}
-?>
-
-
-</<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html lang="en">
 <head>
-    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-    <title>Document</title>
+    <meta charset="UTF-8">
+    <title>فرم پیشنهاد سوال</title>
+    <link href="styles.css" rel="stylesheet" type="text/css">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" rel="stylesheet"/>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min.js"></script>
 </head>
+
+
 <body>
-<h3 dir="rtl">اگر میخواهید با اکانت پیشفرض سوال ها اضافه شوند
-    username و password
-    را خالی بگذارید</h3><br>
-<form action="addPrivateContest.php" method="post">
-    <input style="width: 20em;" name="username" value="" placeholder="username"><br>
-    <input style="width: 20em;" name="password" value="" type="password" placeholder="password"><br>
-    <input style="width: 20em;" name="contestAddressPrefix" value="gym" placeholder="contest address prefix"><br>
-    <input style="width: 20em;" name="contestId" value="" placeholder="contest id"><br>
-    <input style="width: 20em;" name="fromProblem" value="A" placeholder="from problem index"><br>
-    <input style="width: 20em;" name="toProblem" value="F" placeholder="to problem index"><br>
-    <input style="width: 20em;" name="prior" value="" placeholder="prior"><br>
-    <input style="width: 20em;" name="additionalTags" value=""
-           placeholder="additional tags (split tags by , )"><br>
-    <input style="width: 20em;" name="defaultDifficulty" value="2400" placeholder="default difficulty"><br>
-    <input class="submit" type="submit" value="Submit">
-</form>
-<h4 dir="rtl">
-    لیست تگ ها:<br><br>
-    <?php
-    echo implode("\n<br> ", json_decode(file_get_contents("data/allTags.txt"), true));
-    ?>
-</h4>
+<noscript>Sorry, your browser does not support JavaScript!</noscript>
+
+<div class="container">
+
+    <form id="contact" action="addPrivateContest.php" method="post">
+        <h3 dir="rtl">فرم پیشنهاد سوال</h3>
+        <script type="text/javascript">
+            $(document).ready(function () {
+                $('.js-example-basic-multiple').select2();
+            });
+        </script>
+
+        <br>
+        <?php
+        $inputs = array("username", "password", "contestAddressPrefix", "contestId", "defaultDifficulty", "fromProblem", "toProblem", "prior");
+
+        ini_set('display_errors', 1);
+        error_reporting(E_ALL);
+
+        try {
+            chdir('..');
+            require __DIR__ . '/../data/defines.php';
+            require __DIR__ . '/../Models/problemset.php';
+            require __DIR__ . '/../Models/CodeforcesUserApi.php';
+
+            foreach ($inputs as $input) {
+                if (!isset($_POST[$input])) {
+                    throw new Exception("_POST input error");
+                }
+            }
+            echo "<h4 dir='rtl'> <b>شروع عملیات...</b></h4><br>";
+            if (!is_numeric($_POST["prior"]) || (float)$_POST["prior"] < 0 || (float)$_POST["prior"] > 0.5) {
+                throw new Exception("prior باید بین ۰ و ۰.۵ باشد!");
+            }
+            $api = new CodeforcesUserApi();
+            if (strlen($_POST["username"]) > 0) {
+                $api->login($_POST["username"], $_POST["password"]);
+            } else {
+                $api->login(CODEFORCES_USERNAME, CODEFORCES_PASSWORD);
+            }
+            $L = ord($_POST["fromProblem"]) - ord('A');
+            $R = ord($_POST["toProblem"]) - ord('A');
+            if ($L < 0 || $L >= 26) {
+                throw new Exception("(from problem index) به درستی وارد نشده است!");
+            }
+            if ($R < $L || $R >= 26) {
+                throw new Exception("(to problem index) به درستی وارد نشده است!");
+            }
+            $problemQueries = $api->getContestProblemQueries($_POST["contestId"], $_POST["contestAddressPrefix"]);
+            problemset::readFromFile();
+            $allTags = json_decode(file_get_contents("data/allTags.txt"), true);
+            $additionalTags=array();
+            if(isset($_POST['additionalTags'])) {
+                $additionalTags = $_POST['additionalTags'];
+            }
+            for ($i = $L; $i <= $R; $i++) {
+                if (!isset($problemQueries[$i])) {
+                    throw new Exception("خطا در دریافت اطلاعات سوال " . chr($i + ord('A')));
+                }
+            }
+
+            for ($i = $L; $i <= $R; $i++) {
+                $data = $api->getProblemArrayData($problemQueries[$i]);
+                if (!isset($data['rating'])) {
+                    $data['rating'] = $_POST['defaultDifficulty'];
+                }
+                $problemId = problemset::addProblem($problemQueries[$i],
+                    array_values(array_unique(array_merge(json_decode($data['tags'], true), $additionalTags)))
+                    , (int)$data['rating'], (float)$_POST["prior"], false);
+                if (strlen($_POST["username"]) > 0) {
+                    problemset::addUserLiked(strtolower($_POST["username"]), $problemId);
+                }
+            }
+            echo "<br>";
+        } catch (Exception $e) {
+            if ($e->getMessage() != "_POST input error") {
+                echo "<errorbox><h4 dir=\"rtl\"> <b>خطا:</b> " . $e->getMessage() . "</h4></errorbox><br>";
+            }
+        }
+        ?>
+        <fieldset>
+            <input name="username" type="text" value="" placeholder="Codeforces Username" tabindex="1" autofocus>
+            <input name="password" value="" type="password" placeholder="Codeforces Password" tabindex="2">
+
+            <h4 dir="rtl">
+                <b>توضیحات: </b>
+                اگر میخواهید با اکانت پیشفرض سوال ها اضافه شوند این دو ورودی را خالی بگذارید
+                <br>
+                برای اضافه کردن سوال از گروه ها و مشاپ ها نیاز است که نقش Manager را در آن ها داشته باشید و این دو
+                وروری
+                را وارد کنید
+                همچنین برای ثبت لایک سوالات این دو ورودی ضروری است
+
+            </h4>
+        </fieldset>
+        <fieldset>
+            http://codeforces.com/[Contest Path]/[Contest Id]
+            <input type="text" name="contestAddressPrefix" value="gym" placeholder="Contest Path" tabindex="3"
+                   required>
+            <h4 dir="rtl"><b>توضیحات: </b> آدرس کانتست: <br>برای مثال:
+                <br>gym<br>contest<br>group/W2YvE0cOoh/contest<br></h4>
+        </fieldset>
+        <fieldset>
+            <input type="number" name="contestId" value="" placeholder="Contest Id" tabindex="3" required>
+            <h4 dir="rtl"><b>توضیحات: </b> شناسه کانتست <br>برای مثال: <br>842<br>280426<br></h4>
+        </fieldset>
+        <fieldset>
+            <input type="text" name="fromProblem" value="A" placeholder="From Problem">
+            <input type="text" name="toProblem" value="F" placeholder="To Problem">
+            <h4 dir="rtl"><b>توضیحات: </b> بازه سوالاتی که قصد اضافه کردن آنها را دارید<br></h4>
+
+        </fieldset>
+        <fieldset>
+            <input name="prior" value="0.2" step="0.01" type="number" min="0" max="0.5"
+                   placeholder="Learning Level">
+            <h4 dir="rtl"><b>توضیحات: </b> میزان جذابیت و آموزنده بودن سوال<br>باید عددی بین ۰ و ۰.۵ باشد</h4>
+        </fieldset>
+        <fieldset>
+            <select class="js-example-basic-multiple" name="additionalTags[]" multiple="multiple">
+                <?php
+                $array = json_decode(file_get_contents("data/allTags.txt"), true);
+                foreach ($array as $tag) {
+                    echo "<option name='$tag'>$tag</option>";
+                }
+                ?>
+            </select>
+            <h4 dir="rtl"><b>توضیحات: </b> تگ های اضافی<br></h4>
+        </fieldset>
+        <fieldset>
+            <input name="defaultDifficulty" type="number" min="700" max="3800" value="2400"
+                   placeholder="Default Difficulty">
+            <h4 dir="rtl"><b>توضیحات: </b> سختی پیشفرض سوالات<br>اگر سوالی در Codeforces دارای سختی نبود, ازین مقدار
+                استفاده میشود<br></h4>
+        </fieldset>
+        <fieldset>
+            <button class="submit" type="submit">Submit</button>
+        </fieldset>
+    </form>
+
+</div>
 </body>
 </html>
