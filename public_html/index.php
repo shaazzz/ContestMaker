19f87b13d70c5acd750ab202d8a3d3d503eb5289
ghmikaeel@gmail.com <ghmikaeel@gmail.com>
2020-05-23 19:12:44 +0430

    fix dot in link path bug

--- public_html/index.php
+++ public_html/index.php
@@ -3,8 +3,8 @@
 $request = strtolower($_SERVER['REQUEST_URI']);
 
 $types = array(
-    array("pattern" => "/^\/ranking|\/$/", "address" => "views/ranking.php"),
-    array("pattern" => "/^\/profile\/(.+)$/", "address" => "views/report.php"),
+    array("pattern" => "/^(\/ranking|\/)$/", "address" => "views/ranking.php"),
+    array("pattern" => "/^\/profile\/([^\/]+)[\/]{0,1}$/", "address" => "views/report.php"),
     array("pattern" => "/^\/suggestproblems\/(.+)$/", "address" => "views/suggestProblems.php"),
     array("pattern" => "/^.+$/", "address" => "views/404.php"),
 );
@@ -14,7 +14,7 @@ require_once 'data/defines.php';
 
 foreach ($types as $tp) {
     if (preg_match($tp['pattern'], $request, $matches)) {
-        $_GET['input'] = $matches[1];
+        $_GET['input'] = str_replace("+", ".", $matches[1]);
         require_once $tp['address'];
         exit(0);
     }
