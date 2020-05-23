ea65bba12638f9a6201f0da69f3fe8173134e98f
ghmikaeel@gmail.com <ghmikaeel@gmail.com>
2020-05-23 19:48:25 +0430

    remove styles.css link in report.php

--- public_html/views/report.php
+++ public_html/views/report.php
@@ -17,7 +17,6 @@ if(!isset(AllUsers::$users[$username])){
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" rel="stylesheet"/>
     <script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min.js"></script>
-    <link href="../styles.css" rel="stylesheet" type="text/css">
     <script type="text/javascript">
         window.onload = function () {
             var chart = new CanvasJS.Chart("chartContainer", {
